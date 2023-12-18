45d5b8219922141975bfaee788fb8cad748ad8db
omarahmedelnemr <omarahmedelnemr16@gmail.com>
2023-12-18 20:05:54 +0200

    Supervisors Init

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginForm from './pages/login/Login';
 import Dashboard from './pages/Dashboard/Dashboad';
 import './components/FontawesomeIcons'
-import SideNavBar from './components/SideNavBar';
+import SideNavBar from './pages/Dashboard/Components/SideNavBar';
 import './components/styles/general.css'
+import SupervisorsPage from './pages/Supervisors/Supervisors';
 function App() {
   return (
     <div className="App">
@@ -13,6 +14,7 @@ function App() {
             <Routes>
                 <Route path='/Login' element={<LoginForm/>} />
                 <Route path='/dashboard' element={<Dashboard/>} />
+                <Route path='/supervisors' element={<SupervisorsPage/>} />
             </Routes>
         </Router>
     </div>
---
+++ src/pages/Supervisors/Supervisors.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import './Supervisors.css';
+import logoImage from '../../content/ta3afiLogo.png';
+import globalVar from '../../public Func/globalVar'
+
+function SupervisorsPage() {
+
+  return (
+    <div id="supervisorsPage">
+
+    </div>
+  );
+}
+
+export default SupervisorsPage;
