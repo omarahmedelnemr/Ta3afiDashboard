b47d53f4a0f6d73268a13c903d800a26dd795faa
OmarElnemr <omarahmedelnemr16@gmail.com>
2024-02-03 20:29:16 +0200

    Adding Tests Routes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import './components/FontawesomeIcons'
 import SideNavBar from './pages/Dashboard/Components/SideNavBar';
 import './components/styles/general.css'
 import SupervisorsPage from './pages/Supervisors/Supervisors';
+import TestChatPage from './pages/test/TestChat';
+import TestNotifyPage from './pages/test/TestNotify';
 function App() {
   return (
     <div className="App">
@@ -15,6 +17,8 @@ function App() {
                 <Route path='/Login' element={<LoginForm/>} />
                 <Route path='/dashboard' element={<Dashboard/>} />
                 <Route path='/supervisors' element={<SupervisorsPage/>} />
+                <Route path='/test' element={<TestChatPage/>} />
+                <Route path='/testnotify' element={<TestNotifyPage/>} />
             </Routes>
         </Router>
     </div>
