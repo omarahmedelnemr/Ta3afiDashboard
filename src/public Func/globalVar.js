a045129ef6376de1a874e3ab992e16a1f4173304
OmarElnemr <omarahmedelnemr16@gmail.com>
2024-03-11 06:09:15 +0200

    add Public Link

--- src/public Func/globalVar.js
+++ src/public Func/globalVar.js
@@ -1,6 +1,7 @@
 const globalVar = {
     // backendURL :"http://192.168.1.9:8000",
-    backendURL :"http://localhost:8000",
+    // backendURL :"http://localhost:8000",
+    backendURL :"http://34.72.8.253",
     // backendURL : "http://35.184.77.94"
 }
 
