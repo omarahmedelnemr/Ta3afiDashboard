70e5d08c2b3dc4f602ece4c4a1fad5addd2f5256
OmarElnemr <omarahmedelnemr16@gmail.com>
2024-03-12 04:05:38 +0200

    Dashboard Done UI

--- src/pages/Dashboard/Dashboad.js
+++ src/pages/Dashboard/Dashboad.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import './Dashboard.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import StatusBoxes from './Components/overviewBoxes';
+import StatusBoxes from './Components/overviewBox';
+import GraphBox from './Components/GraphBox';
+import IssusBox from './Components/IssuesBox';
+import TimeGraphBox from './Components/TimeGraphBox';
 
 function Dashboard() {
   // Check Authentication
@@ -11,7 +14,18 @@ function Dashboard() {
     
 return (
     <div id='DashboardPage'>
-        <StatusBoxes/>
+        <div className='row infoBoxes'>
+          <StatusBoxes title={"Patients"} icon={<FontAwesomeIcon icon="fa-solid fa-user" />} color={"#d798eb"} value={23}/>
+          <StatusBoxes title={"Doctors"} icon={<FontAwesomeIcon icon="fa-solid fa-user-doctor" />} color={"#ee9981"} value={23}/>
+          <StatusBoxes title={"Articles"} icon={<FontAwesomeIcon icon="fa-solid fa-newspaper" />} color={"#67a5e7"} value={23}/>
+          <StatusBoxes title={"Posts"} icon={<FontAwesomeIcon icon="fa-solid fa-square-poll-horizontal" />} color={"#f4c075"} value={23}/>
+        </div>           
+        <div className='row SecondLevel'>
+          <GraphBox title={"Appoinments"}/>
+          <IssusBox title={"Latest Issus"}/>
+        </div>
+        <TimeGraphBox title={"Appointment Over Time"}/>
+
     </div>
 )
 }
