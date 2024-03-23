e6aae695460091ddff797a229da08d0df542ff53
OmarElnemr <omarahmedelnemr16@gmail.com>
2024-03-23 06:05:17 +0200

    Remove GlobalVar

--- src/public Func/globalVar.js
+++
@@ -1,8 +0,0 @@
-const globalVar = {
-    // backendURL :"http://192.168.1.9:8000",
-    // backendURL :"http://localhost:8000",
-    backendURL :"http://34.72.8.253",
-    // backendURL : "http://35.184.77.94"
-}
-
-export default globalVar;
