2c19c7d2f1b730b5d0b97656757c6bc6872785eb
OmarElnemr <omarahmedelnemr16@gmail.com>
2024-07-14 21:42:12 +0300

    Profile Page Done

--- src/pages/Profiles/DoctorProfile.js
+++ src/pages/Profiles/DoctorProfile.js
@@ -1,71 +1,270 @@
 import React from 'react';
 import './UserProfile.css';
 import { useParams } from 'react-router';
+import axios from 'axios';
+import globalVar from '../../public Func/globalVar';
+import { useState, useEffect } from "react";
 
 function DoctorProfile() {
     const {doctorID} = useParams()
-    
+        // Main Info
+        const [profilePic,setProfilePic] = useState(null)
+        const [name,setName] = useState(null)
+        const [title,setTitle] = useState(null)
+        const [birthDate,setBirthDate] = useState(null)
+        const [email,setEmail] = useState(null)
+        const [gender,setGender] = useState(null)
+        const [description,setDescription] = useState(null)
+        const [online,setOnline] = useState(null)
+        const [starRate,setStarRate] = useState(null)
+        const [completedSessions,setCompletedSessions] = useState(null)
+        const [pendingSessions,setPendingSessions] = useState(null)
+
+
+        // Account Details
+        const [education,setEducation] = useState(<p className="commentKey">No Education Records Found</p>)
+        const [experince,setExperince] = useState(<p className="commentKey">No Experince Records Found</p>)
+        const [certificate,setCertificate] = useState(<p className="commentKey">No Certificate Records Found</p>)
+        const [tag,setTag] = useState(<p className="commentKey">No Tags Found</p>)
+        const [price,setPrice] = useState(<p className="commentKey">No Prices Found</p>)
+        const [availableTime,setAvailableTime] = useState(<p className="commentKey">No Times Found</p>)
+
+        useEffect(()=>{
+            // Getting The doctor Info
+            axios.get(`${globalVar.backendURL}/profile/doctor-main-info?doctorID=${doctorID}`).then(res=>{
+                setProfilePic(res.data.profileImage)
+                setName(res.data.name)
+                setTitle(res.data.title)
+                setBirthDate(res.data.birthDate?res.data.birthDate.split("T")[0]:" - ")
+                setEmail(res.data.email)
+                setGender(res.data.gender)
+                setDescription(res.data.description)
+                setOnline(res.data.online)
+                setStarRate(res.data.starRate)
+                setCompletedSessions(res.data.completedSessions)
+                setPendingSessions(res.data.pendingSessions)
+            }).catch(err=>{
+                console.log("Error!",err)
+            })
+
+            // Getting The doctor Education
+            axios.get(`${globalVar.backendURL}/profile/doctor-education-record?doctorID=${doctorID}`).then(res=>{
+                const data = res.data
+                const temp = []
+                for (var i=0;i<data.length;i++){
+                    temp.push(
+                            <div className='card'>
+                                <span className={'key title'}>Title: {data[i]['title']}</span>&nbsp;
+                                <span className={'key place'}>From: {data[i]['place']}</span>&nbsp;
+                                <span className={'key startDate'}>Started: {data[i]['startDate'] ? data[i]['startDate'].split("T")[0]:null}</span>&nbsp;
+                                <span className={'key endDate'}>Ended: {data[i]['endDate'] ? data[i]['endDate'].split("T")[0]:null}</span>&nbsp;
+                            </div>
+                    
+                    )
+                }
+                setEducation(
+                    temp.length?
+                    <div className={"DataEntry"}>
+                        {temp}
+                    </div>
+                    :
+                    <p className="commentKey">No Education Records Found</p>
+
+                )
+            }).catch(err=>{
+                console.log("Error!",err)
+            })
+            
+            // Getting The doctor experince
+            axios.get(`${globalVar.backendURL}/profile/doctor-experince-record?doctorID=${doctorID}`).then(res=>{
+                const data = res.data
+                const temp = []
+                for (var i=0;i<data.length;i++){
+                    temp.push(
+                            <div className='card'>
+                                <span className={'key title'}>Title: {data[i]['title']}</span>&nbsp;
+                                <span className={'key place'}>From: {data[i]['place']}</span>&nbsp;
+                                <span className={'key startDate'}>Started: {data[i]['startDate'] ? data[i]['startDate'].split("T")[0]:null}</span>&nbsp;
+                                <span className={'key endDate'}>Ended: {data[i]['endDate'] ? data[i]['endDate'].split("T")[0]:null}</span>&nbsp;
+                            </div>
+                    
+                    )
+                }
+                setExperince(
+                    temp.length?
+                    <div className={"DataEntry"}>
+                        {temp}
+                    </div>
+                    :
+                    <p className="commentKey">No Experince Records Found</p>
+
+                )
+            }).catch(err=>{
+                console.log("Error!",err)
+            })
+
+            // Getting The doctor Certificates
+            axios.get(`${globalVar.backendURL}/profile/doctor-certificate-record?doctorID=${doctorID}`).then(res=>{
+                const data = res.data
+                const temp = []
+                for (var i=0;i<data.length;i++){
+                    temp.push(
+                            <div className='card'>
+                                <span className={'key title'}>Title: {data[i]['title']}</span>&nbsp;
+                                <span className={'key place'}>From: {data[i]['place']}</span>&nbsp;
+                                <span className={'key date'}>Date: {data[i]['date'] ? data[i]['date'].split("T")[0]:null}</span>&nbsp;
+                            </div>
+                    
+                    )
+                }
+                setCertificate(
+                    temp.length?
+                    <div className={"DataEntry"}>
+                        {temp}
+                    </div>
+                    :
+                    <p className="commentKey">No Certificates Records Found</p>
+                )
+            }).catch(err=>{
+                console.log("Error!",err)
+            })
+
+            // Getting The doctor Tags
+            axios.get(`${globalVar.backendURL}/profile/doctor-tag?doctorID=${doctorID}`).then(res=>{
+                const data = res.data
+                const temp = []
+                for (var i=0;i<data.length;i++){
+                    temp.push(
+                        <span className={'key'}>{data[i]['tag']}</span>                    
+                    )
+                }
+                setTag(
+                    temp.length?
+                    <div className={"DataEntry"}>
+                        {temp}
+                        
+                    </div>
+                    :
+                    <p className="commentKey">No Tags Found</p>
+                )
+            }).catch(err=>{
+                console.log("Error!",err)
+            })
+
+            // Getting The doctor Prices
+            axios.get(`${globalVar.backendURL}/profile/doctor-price?doctorID=${doctorID}`).then(res=>{
+                const data = res.data
+                const temp = []
+                for (var i=0;i<data.length;i++){
+                    temp.push(
+                        <div className={"card"}>
+                            <p className='key'>Minuts: <span className='commentKey'>{data[i]["minutesRate"]}</span></p>
+                            <p className='key'>Price: <span className='commentKey'>{data[i]["moneyRate"]}</span></p>
+                        </div>
+                    
+                    )
+                }
+                setPrice(
+                    temp.length?
+                    <div className={"DataEntry"}>
+                        {temp}
+                    </div>
+                    :
+                    <p className="commentKey">No Prices Found</p>
+
+                )
+            }).catch(err=>{
+                console.log("Error!",err)
+            })
+
+            // Getting The doctor times
+            axios.get(`${globalVar.backendURL}/profile/doctor-available-time?doctorID=${doctorID}`).then(res=>{
+                const data = res.data
+                const temp = []
+                for (var i=0;i<data.length;i++){
+                    temp.push(
+                            <div className='card'>
+                                <span className={'key title'}>Day: {data[i]['dayName']}</span>&nbsp;
+                                <div>
+                                    {
+                                        data[i]['hours'].map(hour=><span className={'key startDate'}>- {hour['hour']} {hour["AMPM"]} - </span>)
+
+                                    }
+                                </div>
+                            </div>
+                    
+                    )
+                }
+                setAvailableTime(
+                    temp.length?
+                    <div className={"DataEntry"}>
+                        {temp}
+                    </div>
+                    :
+                    <p className="commentKey">No Times Found</p>
+
+                )
+            }).catch(err=>{
+                console.log("Error!",err)
+            })
+        },[])
     return (
         <div id="UserProfile">
             <div id='UserMain'>
-                <img className='ProfileImage' src={"http://localhost:8000/profilepic/default.png"} />
+                <img className='ProfileImage' src={profilePic} title='Profile Picture'/>
                 <div className='textMain'>
-                    <h2>Omar Ahmed Elnemr</h2>
-                    <h3>omar@email.com</h3>
-                    <h3>Psychiatry</h3>
-                    <p>Male</p>
+                    <h2>{name}</h2>
+                    <h3>{email}</h3>
+                    <h3>{title}</h3>
+                    <p>{gender}</p>
+                    <p className={"mainInfo"}>Birth Date:  <span>{birthDate}</span></p>
+                    <p className={"mainInfo"}>Description:  <span>{description}</span></p>
+                    <p className={"mainInfo"}>Status: &nbsp;
+                        {online?
+                            <div className={"doctorStatus online"}></div>
+                        :                        
+                            <div className={"doctorStatus offline"}></div>
+                        } 
+                         <span> {online?"online":"offline"}</span>
+                    </p>
+                    <p className={"mainInfo"}>Star Rate: <span>{starRate}</span></p>
+                    <p className={"mainInfo"}>Completed Sessions: <span>{completedSessions}</span></p>
+                    <p className={"mainInfo"}>Pending Sessions: <span>{pendingSessions}</span></p>
                 </div>
                 
             </div>
             <div id='UserData'>
                 <div className='DataSection'>
-                    <h1 className='DataTitle'>Main Info</h1>
+                    <h1 className='DataTitle'>Education</h1>
                     <hr />
-                    <div className='DataEntry'>
-                        <p className='key'>Height</p>
-                        <p>172cm</p>
-                    </div>
-                    <div className='DataEntry'>
-                        <p className='key'>Weight</p>
-                        <p>90 KG</p>
-                    </div>
-                    <div className='DataEntry'>
-                        <p className='key'>Blood</p>
-                        <p>null</p>
-                    </div>
-                    <div className='DataEntry'>
-                        <p className='key'>martialStatus</p>
-                        <p>null</p>
-                    </div>
-                </div>
-                <div className='DataSection'>
-                    <h1 className='DataTitle'>Hobbies</h1>
-                    <hr />
-                    <div className='DataEntry'>
-                        <p className='key'>Tennis</p>
-                    </div>
-                    <div className='DataEntry'>
-                        <p className='key'>Basket Ball</p>
-                    </div>
-                    <div className='DataEntry'>
-                        <p className='key'>Football</p>
-                    </div>
                     
+                    {education}
                 </div>
                 <div className='DataSection'>
-                    <h1 className='DataTitle'>Diagnosis</h1>
+                    <h1 className='DataTitle'>Experince</h1>
                     <hr />
-                    <div className='DataEntry'>
-                        <p className='key'>Depression</p>
-                    </div>
-                    <div className='DataEntry'>
-                        <p className='key'>OSD</p>
-                    </div>
-                    <div className='DataEntry'>
-                        <p className='key'>Mental Disorder</p>
-                    </div>
+                    {experince}     
                 </div>
-                
+                <div className='DataSection'>
+                    <h1 className='DataTitle'>Certificates</h1>
+                    <hr />
+                    {certificate}
+                </div>
+                <div className='DataSection'>
+                    <h1 className='DataTitle'>Tags</h1>
+                    <hr />
+                    {tag}
+                </div>
+                <div className='DataSection'>
+                    <h1 className='DataTitle'>Prices</h1>
+                    <hr />
+                    {price}
+                </div>
+                <div className='DataSection'>
+                    <h1 className='DataTitle'>Available Time</h1>
+                    <hr />
+                    {availableTime}
+                </div>
             </div>
         </div>
     );
--- src/pages/Profiles/PatientProfile.js
+++ src/pages/Profiles/PatientProfile.js
@@ -1,18 +1,144 @@
 import React from 'react';
 import './UserProfile.css';
 import { useParams } from 'react-router';
+import axios from 'axios';
+import globalVar from '../../public Func/globalVar';
+import { useState, useEffect } from "react";
 
 function PatientProfile() {
     const {patientID} = useParams()
 
+    // Account Info
+    const [profilePic,setProfilePic] = useState(null)
+    const [name,setName] = useState(null)
+    const [email,setEmail] = useState(null)
+    const [gender,setGender] = useState(null)
+
+    // Main Info
+    const [Height,setHeight] = useState(" - ")
+    const [Weight,setWeight] = useState(" - ")
+    const [Blood,setBlood] = useState(" - ")
+    const [martialStatus,setmartialStatus] = useState(" - ")
+    const [alcohol,setAlcohol] = useState(" - ")
+    const [drugs,setDrugs] = useState(" - ")
+    const [religious,setReligious] = useState(" - ")
+    const [religion,setReligion] = useState(" - ")
+
+
+    // Other Sections
+    const [hobbies,setHobbies] = useState([])
+    const [Diagnoses,setDiagnoses] = useState([])
+    const [Medicine,setMedicine] = useState([])
+    const [prescriptionFile,setPrescriptionFile] = useState([])
+
+
+    useEffect(()=>{
+        // Getting The Patient Info
+        axios.get(`${globalVar.backendURL}/profile/patient-main-info?patientID=${patientID}`).then(res=>{
+            setProfilePic(res.data.profileImage)
+            setName(res.data.name)
+            setEmail(res.data.email)
+            setGender(res.data.gender)
+        }).catch(err=>{
+            console.log("Error!",err)
+        })
+
+        // Getting The Patient Personal Account Info
+        axios.get(`${globalVar.backendURL}/profile/patient-account-info?patientID=${patientID}`).then(res=>{
+            const data = res.data
+            setHeight(data['height']? data['height']:<span className='commentKey'>Not Set</span>)
+            setWeight(data['weight']? data['weight']:<span className='commentKey'>Not Set</span>)
+            setBlood(data['blood']? data['blood']:<span className='commentKey'>Not Set</span>)
+            setmartialStatus(data['martialStatus']? data['martialStatus']:<span className='commentKey'>Not Set</span>)
+            setAlcohol(data['alcohol']? data['alcohol']:<span className='commentKey'>Not Set</span>)
+            setDrugs(data['drugs']? data['drugs']:<span className='commentKey'>Not Set</span>)
+            setReligious(data['religious']? data['religious']:<span className='commentKey'>Not Set</span>)
+            setReligion(data['religion']? data['religion']:<span className='commentKey'>Not Set</span>)
+
+        }).catch(err=>{
+            console.log("Error!",err)
+        })
+
+        // Getting The Patient Hobbies
+        axios.get(`${globalVar.backendURL}/profile/patient-hobby?patientID=${patientID}`).then(res=>{
+            console.log(res.data)
+            const data = res.data
+            const temp = []
+            for(var i =0;i< data.length;i++){
+                temp.push(
+                    <div className='DataEntry' key={"hobby"+data[i]['id']}>
+                        <p className='key'>{data[i]["hobby"]}</p>
+                    </div>
+                )
+            }
+            setHobbies(temp.length?temp:<p className='commentKey'>No Hobbies Found</p>)
+        }).catch(err=>{
+            console.log("Error!",err)
+        })
+
+        // Getting The Patient Diagnoses
+        axios.get(`${globalVar.backendURL}/profile/patient-diagnose?patientID=${patientID}`).then(res=>{
+            console.log(res.data)
+            const data = res.data
+            const temp = []
+            for(var i =0;i< data.length;i++){
+                temp.push(
+                    <div className='DataEntry' key={"diagnose"+data[i]['id']}>
+                        <p className='key'>{data[i]['name']}</p>
+                        <span className='drName'>By Dr./ {data[i]['doctorName']}</span>
+                    </div>
+                )
+            }
+            setDiagnoses(temp.length?temp:<p className='commentKey'>No Diagnoses Found</p>)
+        }).catch(err=>{
+            console.log("Error!",err)
+        })
+        
+        // Getting The Patient Medicine
+        axios.get(`${globalVar.backendURL}/profile/patient-medicine?patientID=${patientID}`).then(res=>{
+            console.log(res.data)
+            const data = res.data
+            const temp = []
+            for(var i =0;i< data.length;i++){
+                temp.push(
+                    <div className='DataEntry' key={"medicine"+data[i]['id']}>
+                        <p className='key'>{data[i]['name']}</p>
+                        <span className='drName'>By Dr./ {data[i]['doctorName']}</span>
+                    </div>
+                )
+            }
+            setMedicine(temp.length?temp:<p className='commentKey'>No Medicine Found</p>)
+        }).catch(err=>{
+            console.log("Error!",err)
+        })
+
+        // Getting The Patient Prescription Files
+        axios.get(`${globalVar.backendURL}/profile/patient-medicine?patientID=${patientID}`).then(res=>{
+            console.log(res.data)
+            const data = res.data
+            const temp = []
+            for(var i =0;i< data.length;i++){
+                temp.push(
+                    <div className='DataEntry' key={"file"+data[i]['id']}>
+                        <img className='key' src={`${globalVar.backendURL}/file/${data[i]['file']}`} />
+                        <span className='drName'>By Dr./ {data[i]['doctorName']}</span>
+                    </div>
+                )
+            }
+            setPrescriptionFile(temp.length?temp:<p className='commentKey'>No Files Found</p>)
+        }).catch(err=>{
+            console.log("Error!",err)
+        })
+    },[])
+
     return (
         <div id="UserProfile">
             <div id='UserMain'>
-                <img className='ProfileImage' src={"http://localhost:8000/profilepic/default.png"} />
+                <img className='ProfileImage' src={profilePic} title='Profile Image'/>
                 <div className='textMain'>
-                    <h2>Omar Ahmed Elnemr</h2>
-                    <h3>omar@email.com</h3>
-                    <p>Male</p>
+                    <h2>{name}</h2>
+                    <h3>{email}</h3>
+                    <p>{gender}</p>
                 </div>
                 
             </div>
@@ -22,49 +148,57 @@ function PatientProfile() {
                     <hr />
                     <div className='DataEntry'>
                         <p className='key'>Height</p>
-                        <p>172cm</p>
+                        <p>{Height} cm</p>
                     </div>
                     <div className='DataEntry'>
                         <p className='key'>Weight</p>
-                        <p>90 KG</p>
+                        <p>{Weight} KG</p>
                     </div>
                     <div className='DataEntry'>
                         <p className='key'>Blood</p>
-                        <p>null</p>
+                        <p>{Blood}</p>
                     </div>
                     <div className='DataEntry'>
                         <p className='key'>martialStatus</p>
-                        <p>null</p>
+                        <p>{martialStatus}</p>
+                    </div>
+                    <div className='DataEntry'>
+                        <p className='key'>Alcohol</p>
+                        <p>{alcohol}</p>
+                    </div>
+                    <div className='DataEntry'>
+                        <p className='key'>Drugs</p>
+                        <p>{drugs}</p>
+                    </div>
+                    <div className='DataEntry'>
+                        <p className='key'>Religious</p>
+                        <p>{religious}</p>
+                    </div>
+                    <div className='DataEntry'>
+                        <p className='key'>Religion</p>
+                        <p>{religion}</p>
                     </div>
                 </div>
                 <div className='DataSection'>
                     <h1 className='DataTitle'>Hobbies</h1>
                     <hr />
-                    <div className='DataEntry'>
-                        <p className='key'>Tennis</p>
-                    </div>
-                    <div className='DataEntry'>
-                        <p className='key'>Basket Ball</p>
-                    </div>
-                    <div className='DataEntry'>
-                        <p className='key'>Football</p>
-                    </div>
-                    
+                    {hobbies}
                 </div>
                 <div className='DataSection'>
                     <h1 className='DataTitle'>Diagnosis</h1>
                     <hr />
-                    <div className='DataEntry'>
-                        <p className='key'>Depression</p>
-                    </div>
-                    <div className='DataEntry'>
-                        <p className='key'>OSD</p>
-                    </div>
-                    <div className='DataEntry'>
-                        <p className='key'>Mental Disorder</p>
-                    </div>
+                    {Diagnoses}
                 </div>
-                
+                <div className='DataSection'>
+                    <h1 className='DataTitle'>Medicine</h1>
+                    <hr />
+                    {Medicine}
+                </div>
+                <div className='DataSection'>
+                    <h1 className='DataTitle'>Prescription Files</h1>
+                    <hr />
+                    {prescriptionFile}
+                </div>
             </div>
         </div>
     );
